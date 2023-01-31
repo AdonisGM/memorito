32b44733a2557afe4d196e75aff9d67cb9d10b22
Nguyễn Mạnh Tùng <nmtung.study@gmail.com>
2023-02-01 00:09:03 +0700

    Add mantine-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { MantineProvider, Text } from '@mantine/core';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Text>Welcome to Mantine!</Text>
+    </MantineProvider>
   )
 }
 
