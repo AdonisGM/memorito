66a567a59254f4781463303ddba8acbb9c5a2ee9
Nguyễn Mạnh Tùng <nmtung.study@gmail.com>
2023-02-01 19:28:49 +0700

    Add header

--- frontend/src/pages/landing-page/index.tsx
+++ frontend/src/pages/landing-page/index.tsx
@@ -1,5 +1,17 @@
-const LandingPage = () => {
-  return <p>Landing page</p>
-}
+import classes from './style.module.css';
+import MeHeader from '../../components/me-header';
 
-export default LandingPage
+const LandingPage = () => {
+  return (
+    <div className={classes.main}>
+      <MeHeader/>
+      <div id={'home'} className={classes.home}></div>
+      <div id={'feature'} className={classes.feature}></div>
+      <div id={'plan'} className={classes.plan}></div>
+      <div id={'contact'} className={classes.contact}></div>
+
+    </div>
+  );
+};
+
+export default LandingPage;
