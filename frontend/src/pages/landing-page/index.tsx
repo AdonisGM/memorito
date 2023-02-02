84e0c88debc31fdc0c951bd4ca2bbe6d2b066be4
Nguyễn Mạnh Tùng <nmtung.study@gmail.com>
2023-02-02 12:54:13 +0700

    Update landing page

--- frontend/src/pages/landing-page/index.tsx
+++ frontend/src/pages/landing-page/index.tsx
@@ -9,29 +9,25 @@ const LandingPage = () => {
     <div className={classes.main}>
       <div className={classes.header}><MeHeader/></div>
       <div id={'home'} className={classes.home}>
-        <Container>
+        <Container className={classes.homeContainer} size={'xl'}>
           <div className={classes.titleHome}>
             <Title color={'dark.6'} order={2} fw={'bolder'}>Free for memorization</Title>
             <Space h={'sm'}/>
             <Text size={'sm'}>We provide you with a new service for memorization. Special, it's always free for all
               plans and all
               people. The solution for paid version of Quizlet.</Text>
-          </div>
-          <Space h={'xl'}/>
-          <Center>
+            <Space h={'xl'}/>
             <Button color="green" radius="md">
               Try now!
             </Button>
-          </Center>
-          <Space h={'xl'}/>
-          <Space h={'xl'}/>
+          </div>
           <Image src={HomeImage} height={500} fit={'contain'}/>
         </Container>
       </div>
-      <div id={'feature'} className={classes.feature}></div>
-      <div id={'plan'} className={classes.plan}></div>
-      <div id={'contact'} className={classes.contact}></div>
-      <div id={'faqs'} className={classes.faqs}></div>
+      {/*<div id={'feature'} className={classes.feature}></div>*/}
+      {/*<div id={'plan'} className={classes.plan}></div>*/}
+      {/*<div id={'contact'} className={classes.contact}></div>*/}
+      {/*<div id={'faqs'} className={classes.faqs}></div>*/}
       <MeFooter/>
     </div>
   );
